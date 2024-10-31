b3a0b88052d63fde9d633f13c583728eafcf5262
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2024-10-31 14:56:20 -0700

    updates

--- Formula/hello-cli.rb
+++ Formula/hello-cli.rb
@@ -2,7 +2,7 @@ class HelloCli < Formula
   desc "Simple CLI tool that prints hello"
   homepage "https://github.com/NicoHinderling/test-homebrew-ruby-pkg-distribution"
   url "https://github.com/NicoHinderling/test-homebrew-ruby-pkg-distribution/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "v0.0.1"  # You'll fill this in after creating the release
+  sha256 "f5af3d33e30a8a0fe24dade0067f305430978aa7a10e0cc62368871ee4dcda88"  # can be gotten from the command "shasum -a 256 v0.0.1.tar.gz" if you download it from the "main" repo
   license "MIT"
 
   depends_on "ruby"
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # homebrew-test-homebrew-ruby-pkg-distribution
 
 This is just for distribution of the test CLI tool: https://github.com/NicoHinderling/test-homebrew-ruby-pkg-distribution
+
+For the actual tool, we can allow users to get it without having to use our specific tap by doing the following:
+
+Submit to Homebrew/core:
+
+This is the main Homebrew repository
+More rigorous review process
+Better for widely-used tools
+You'd need to submit a pull request to: https://github.com/Homebrew/homebrew-core
